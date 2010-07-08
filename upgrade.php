34f7a6a1489bdc929861f6ad33075c1695fb5670
Chad Smith <chad@nospam.me>
2010-07-08 02:10:49 -0500

    first commit

---
+++ upgrade.php
@@ -0,0 +1,54 @@
+<?php
+if(!OpenVBX::isAdmin())
+	die();
+$cwd=dirname(__FILE__);
+$tmp=$cwd.'/tmp/';
+$root=dirname(dirname($cwd));
+if(!is_writable($root))
+	die($root.' must be writable by '.get_current_user());
+if(!is_writable($tmp))
+	die($tmp.' must be writable by '.get_current_user());
+$zip=$tmp.'zip';
+file_put_contents($zip,file_get_contents('http://github.com/twilio/OpenVBX/zipball/master'));
+$z=new ZipArchive;
+if(true===$z->open($zip)){
+    $z->extractTo($tmp);
+    $z->close();
+	@unlink($zip);
+	$d=dir($tmp);
+	while($file=$d->read())
+		if(!in_array($file,array('.','..'))){
+			mvdir($tmp.$file,$root);
+			rmall($tmp.$file);
+		}
+	$d->close();
+	die('Success!');
+}
+die('Upgrade failed.');
+function mvdir($source,$target){
+	if(is_dir($source)){
+		@mkdir($target);
+		$d=dir($source);
+		while($file=$d->read())
+			if(!in_array($file,array('.','..'))){
+				$path=$source.'/'.$file;
+				if(is_dir($path))
+					mvdir($path,$target.'/'.$file);
+				else
+					@rename($path,$target.'/'.$file);
+			}
+		$d->close();
+	}else
+		@rename($source,$target);
+}
+function rmall($dir){
+	$files=glob($dir.'*',GLOB_MARK);
+	foreach($files as $file){
+		if(is_dir($file))
+			rmall($file);
+		else
+			@unlink($file);
+	}
+	if(is_dir($dir))
+		@rmdir($dir);
+}
