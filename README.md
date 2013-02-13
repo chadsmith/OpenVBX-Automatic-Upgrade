c6acf266f1b9dc5f700385aa12b45d830112a580
Chad Smith <chad@nospam.me>
2013-02-12 20:30:30 -0600

    use Phar class is ZipArchive is not installed

---
+++ README.md
@@ -0,0 +1,16 @@
+# One-click updates for OpenVBX
+
+In order for this plugin to work, the [ZipArchive][1] or [Phar][2] class will need to be installed, and the root directory for OpenVBX will need to be group writable by the local web user.
+
+[1]: http://www.php.net/manual/en/book.zip.php
+[2]: http://www.php.net/manual/en/book.phar.php
+
+## Installation
+
+[Download][3] the plugin and extract to /plugins
+
+[3]: https://github.com/chadsmith/OpenVBX-Automatic-Upgrade/archives/master
+
+## Usage
+
+When signed in as an Admin user, click "Automatic Upgrade" in the System menu of the OpenVBX sidebar.
--- upgrade.php
+++ upgrade.php
@@ -1,49 +1,65 @@
 <?php
 if(!OpenVBX::isAdmin())
 	die();
-$cwd=dirname(__FILE__);
-$tmp=$cwd.'/tmp/';
-$root=dirname(dirname($cwd));
+if(!class_exists('ZipArchive') && !class_exists('Phar'))
+  die('The <a href="http://www.php.net/manual/en/book.zip.php">ZipArchive</a> or <a href="http://www.php.net/manual/en/book.phar.php">Phar</a> class must be installed to use this plugin.');
+$cwd = dirname(__FILE__);
+$tmp = $cwd . '/tmp/';
+$root = dirname(dirname($cwd));
 if(!is_writable($root))
-	die($root.' must be writable by '.get_current_user());
+	die($root . ' must be writable by ' . get_current_user());
 if(!is_writable($tmp))
-	die($tmp.' must be writable by '.get_current_user());
-$zip=$tmp.'zip';
-file_put_contents($zip,file_get_contents('https://github.com/twilio/OpenVBX/zipball/master'));
-$z=new ZipArchive;
-if(true===$z->open($zip)){
+	die($tmp . ' must be writable by ' . get_current_user());
+$archive = $tmp . 'archive';
+if(class_exists('ZipArchive')) {
+  file_put_contents($archive, file_get_contents('https://github.com/twilio/OpenVBX/zipball/master'));
+  $z = new ZipArchive;
+  if(true === $z->open($archive)) {
     $z->extractTo($tmp);
     $z->close();
-	@unlink($zip);
-	$d=dir($tmp);
-	while($file=$d->read())
-		if(!in_array($file,array('.','..'))){
-			mvdir($tmp.$file,$root);
-			rmall($tmp.$file);
-		}
-	$d->close();
-	die('Success!');
+    unlink($archive);
+  }
+  else
+    die('Upgrade failed.');
 }
-die('Upgrade failed.');
-function mvdir($source,$target){
-	if(is_dir($source)){
+else {
+  file_put_contents($archive, file_get_contents('https://github.com/twilio/OpenVBX/tarball/master'));
+  $p = new Phar($archive);
+  if(true === $p->extractTo($tmp))
+    unlink($archive);
+  else
+    die('Upgrade failed.');
+}
+$d = dir($tmp);
+while($file = $d->read())
+	if(!in_array($file, array('.', '..'))) {
+		mvdir($tmp . $file, $root);
+		rmall($tmp . $file);
+	}
+$d->close();
+die('Success!');
+
+function mvdir($source, $target) {
+	if(is_dir($source)) {
 		@mkdir($target);
-		$d=dir($source);
-		while($file=$d->read())
-			if(!in_array($file,array('.','..'))){
-				$path=$source.'/'.$file;
+		$d = dir($source);
+		while($file = $d->read())
+			if(!in_array($file, array('.', '..'))) {
+				$path = $source . '/' . $file;
 				if(is_dir($path))
-					mvdir($path,$target.'/'.$file);
+					mvdir($path, $target . '/' . $file);
 				else
-					@rename($path,$target.'/'.$file);
+					@rename($path, $target . '/' . $file);
 			}
 		$d->close();
-	}else
-		@rename($source,$target);
+	}
+	else
+		@rename($source, $target);
 }
-function rmall($dir){
-	$files=glob($dir.'*',GLOB_MARK);
-	foreach($files as $file){
+
+function rmall($dir) {
+	$files = glob($dir . '*', GLOB_MARK);
+	foreach($files as $file) {
 		if(is_dir($file))
 			rmall($file);
 		else
